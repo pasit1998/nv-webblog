a233d545b2d427f58d1253bc08bc2678877bf103
pasit1998 <pasits64@nu.ac.th>
2023-08-16 21:59:34 +0700

    install express

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+let express = require ('express')
+const app = express()
+
+app.get('/status', function (req,res){
+    res.send('hello')
+})
+
+let port = 8081
+app.listen(port, function () {
+ console.log('server running on ' + port)
+})
